6f3804ea40c5dd53a1304e4d3c867c74a5d67002
Chris J Mears <chris.mears@gmail.com>
2022-10-15 11:03:45 -0700

    Add multiple cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,158 @@
 <script lang="ts">
-	const selectedCard = [
-		['Cold brew', 'Total new to you', 'Taiwan', 'Dark roast', 'With a friend'],
-		['Mix 2 teas', 'Sample size', 'Floral', 'Gaiwan brew', 'Favorite vendor'],
-		['Fruity', 'Currently dislike', 'Read about tea making', 'Reach 5+ steeps', 'With music'],
-		['Gifted by a friend', 'China', 'With a meal', 'Toasty', 'Bowl brew'],
-		['Texture', 'Sparkling brew', 'Side by side', 'Favorite teaware', 'Outside Taiwan & China']
+	import clone from 'just-clone';
+
+	interface Card {
+		id: string;
+		name: string;
+		backgroundGradient: string;
+		cardLabelClass: string;
+		data: string[][];
+	}
+
+	interface Tab {
+		id: string;
+		label: string;
+	}
+
+	const availableCards: Record<string, Card> = {
+		mist: {
+			id: 'mist',
+			name: 'Mist Card',
+			backgroundGradient: 'from-cyan-300 to-cyan-500',
+			cardLabelClass: 'text-cyan-300',
+			data: [
+				['Cold brew', 'Total new to you', 'Taiwan', 'Dark roast', 'With a friend'],
+				['Mix 2 teas', 'Sample size', 'Floral', 'Gaiwan brew', 'Favorite vendor'],
+				['Fruity', 'Currently dislike', 'Read about tea making', 'Reach 5+ steeps', 'With music'],
+				['Gifted by a friend', 'China', 'With a meal', 'Toasty', 'Bowl brew'],
+				['Texture', 'Sparkling brew', 'Side by side', 'Favorite teaware', 'Outside Taiwan & China']
+			]
+		},
+		woodfire: {
+			id: 'woodfire',
+			name: 'Woodfire',
+			backgroundGradient: 'from-stone-300 to-stone-500',
+			cardLabelClass: 'text-stone-300',
+			data: [
+				['Aged oolong', 'Bug bitten', 'Same tea 3 ways', 'High oxidation', 'Tea and food pairing'],
+				[
+					'Unusual cultivar',
+					'High mtn.',
+					'Outside Taiwan & China',
+					'Dancong',
+					'Gifted by a friend'
+				],
+				['Low elevation', 'Mix 2 teas', 'Wuyi', 'Old bush', 'Same cultivar, two teas'],
+				['One tea 3 cups', 'Side by side', 'Sparkling brew', 'Anxi', 'Light roast'],
+				[
+					'Currently dislike',
+					'Quin Xin cultivar',
+					'Dark roast',
+					'Totally new to you',
+					'Home roasting'
+				]
+			]
+		},
+		nightShift: {
+			id: 'nightShift',
+			name: 'Night Shift',
+			backgroundGradient: 'from-slate-300 to-slate-500',
+			cardLabelClass: 'text-slate-300',
+			data: [
+				['Gifted by a friend', 'With music', 'Late night', 'Favorite vendor', 'Gaiwan brew'],
+				['Brew bowl', 'Roasted tea', 'With friends', 'Home roasting', 'Old tea'],
+				['Totally new to you', 'Cold brew', 'Hard at work', 'Mix 2 teas', 'Early morning'],
+				['After a long day', 'While writing', 'Tea break', 'On the light side', 'High elevation'],
+				['Same tea 3 ways', 'Favorite teaware', 'Early morning', 'Texture', 'Reach 5+ steeps']
+			]
+		}
+	};
+
+	const tabs: Tab[] = Object.values(availableCards).map((tab) => {
+		return {
+			id: tab.id,
+			label: tab.name
+		};
+	});
+
+	const emptySelection: boolean[][] = [
+		new Array(5),
+		new Array(5),
+		new Array(5),
+		new Array(5),
+		new Array(5)
 	];
 
-	let currentSelection = [new Array(5), new Array(5), new Array(5), new Array(5), new Array(5)];
+	let currentSelection = clone(emptySelection);
+	let selectedTab = 'mist';
+
+	$: backgroundGradient = availableCards[selectedTab ?? 0].backgroundGradient;
+	$: cardName = availableCards[selectedTab ?? 0].name;
+	$: cardLabelClass = availableCards[selectedTab ?? 0].cardLabelClass;
 </script>
 
-<div class="bg-gradient-to-b from-cyan-300 to-cyan-500">
+<div class="bg-gradient-to-b {backgroundGradient}">
 	<div class="max-w-2xl mx-auto p-4">
+		<div class="mb-4">
+			<div class="sm:hidden">
+				<label for="tabs" class="sr-only">Select a tab</label>
+				<select
+					id="tabs"
+					name="tabs"
+					class="block w-full rounded-md border-gray-300 focus:border-cyan-500 focus:ring-cyan-500"
+					bind:value={selectedTab}
+					on:change={() => (currentSelection = clone(emptySelection))}
+				>
+					{#each tabs as tab}
+						<option value={tab.id}>{tab.label}</option>
+					{/each}
+				</select>
+			</div>
+			<div class="hidden sm:block">
+				<nav class="flex space-x-4" aria-label="Tabs">
+					{#each tabs as tab}
+						<button
+							class="text-gray-800 hover:text-gray-900 px-3 py-2 font-medium text-sm rounded-md"
+							class:bg-gray-100={tab.id === selectedTab}
+							class:text-gray-700={tab.id === selectedTab}
+							on:click={() => {
+								selectedTab = tab.id;
+								currentSelection = clone(emptySelection);
+							}}>{tab.label}</button
+						>
+					{/each}
+				</nav>
+			</div>
+		</div>
+
 		<h1 class="text-2xl font-semibold text-cyan-50 uppercase text-center tracking-wider">
 			Tea Curious <span class="font-light">Tea Challenge</span>
 		</h1>
 		<h2 class="text-6xl font-bold text-cyan-50 text-center">Moving Oolong!</h2>
 
-		<div class="grid grid-cols-5 gap-3 mt-4 relative">
-      <div class="absolute text-xl font-bold text-white text-cyan-300 uppercase rotate-[270deg] bottom-12 -right-20">Mist Card</div>
-			<div class="col-span-5 p-2 text-center uppercase bg-sky-700 text-white">Start</div>
-			{#each selectedCard as row, i}
-				{#each row as square, j}
-					<button
-						class="flex items-center justify-center text-center h-20 p-2 bg-cyan-200 hover:bg-cyan-300 cursor-pointer font-light focus:outline-cyan-400"
-						class:bg-cyan-700={currentSelection[i][j]}
-						class:text-white={currentSelection[i][j]}
-            class:hover:bg-cyan-700={currentSelection[i][j]}
-						on:click={() => (currentSelection[i][j] = !currentSelection[i][j])}
-					>
-						{selectedCard[i][j]}
-					</button>
+		{#if selectedTab}
+			<div class="grid grid-cols-5 gap-3 mt-4 relative">
+				<div
+					class="absolute text-xl font-bold uppercase rotate-[270deg] bottom-12 -right-20 {cardLabelClass}"
+				>
+					{cardName}
+				</div>
+				<div class="col-span-5 p-2 text-center uppercase bg-sky-700 text-white">Start</div>
+				{#each availableCards[selectedTab]?.data ?? [] as row, i}
+					{#each row as square, j}
+						<button
+							class="flex items-center justify-center text-center h-20 p-2 bg-cyan-200 hover:bg-cyan-300 cursor-pointer font-light focus:outline-cyan-400"
+							class:bg-cyan-700={currentSelection[i][j]}
+							class:text-white={currentSelection[i][j]}
+							class:hover:bg-cyan-700={currentSelection[i][j]}
+							on:click={() => (currentSelection[i][j] = !currentSelection[i][j])}
+						>
+							{availableCards[selectedTab].data[i][j]}
+						</button>
+					{/each}
 				{/each}
-			{/each}
-			<div class="col-span-5 p-2 text-center uppercase bg-emerald-700 text-white">Finish</div>
-		</div>
+				<div class="col-span-5 p-2 text-center uppercase bg-emerald-700 text-white">Finish</div>
+			</div>
+		{/if}
 	</div>
 </div>
