fade00e548dd3c30cdfb0b716c17afa1f756f18f
Chris J Mears <chris.mears@gmail.com>
2022-10-21 23:28:13 -0700

    Make the Square a component

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type { Card } from 'src/cards';
+	import type { Card } from '../cards';
+	import Square from './Square.svelte';
 
 	export let cards: Record<string, Card>;
 	export let selectedCard: string;
@@ -8,19 +9,17 @@
 </script>
 
 <div class="grid grid-cols-5 gap-1 sm:gap-3 mt-4 relative">
-	<div class="col-span-5 p-2 text-center uppercase bg-sky-700 text-white">Start</div>
+	<div class="col-span-5 p-2 rounded text-center uppercase bg-sky-700 text-white">Start</div>
 	{#each cards[selectedCard]?.data ?? [] as row, i}
 		{#each row as square, j}
-			<button
-				class="flex items-center justify-center text-center rounded h-16 sm:h-20 text-xs sm:text-sm p-2 bg-cyan-200 hover:bg-cyan-300 cursor-pointer font-light focus:outline-cyan-400"
-				class:bg-cyan-700={currentSelection[i][j]}
-				class:text-white={currentSelection[i][j]}
-				class:hover:bg-cyan-700={currentSelection[i][j]}
-				on:click={() => handleSelection(i, j)}
-			>
-				{cards[selectedCard].data[i][j]}
-			</button>
+			<Square
+				selected={currentSelection[i][j]}
+				{selectedCard}
+				{handleSelection}
+				row={i}
+				column={j}
+			/>
 		{/each}
 	{/each}
-	<div class="col-span-5 p-2 text-center uppercase bg-emerald-700 text-white">Finish</div>
+	<div class="col-span-5 p-2 rounded text-center uppercase bg-emerald-700 text-white">Finish</div>
 </div>
---
+++ src/components/Square.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { cards } from '../cards';
+
+	export let selected: boolean;
+	export let selectedCard: string;
+	export let handleSelection: (i: number, j: number) => void;
+	export let row: number;
+	export let column: number;
+
+	$: selectedClasses = selected ? '!bg-cyan-700 text-white hover:bg-cyan-700' : '';
+</script>
+
+<button
+	class="flex items-center justify-center text-center rounded h-16 sm:h-20 text-xs sm:text-sm p-2 bg-cyan-200 hover:bg-cyan-300 cursor-pointer font-light focus:outline-cyan-400 {selectedClasses}"
+	on:click={() => handleSelection(row, column)}
+>
+	{cards[selectedCard].data[row][column]}
+</button>
