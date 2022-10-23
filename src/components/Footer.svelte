66a74a59dad3daca2d04837021e96047a81d92d0
Chris J Mears <chris.mears@gmail.com>
2022-10-22 20:37:16 -0700

    Drop shadows

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,10 @@
 <div class="flex flex-col gap-2 text-white mt-4">
-	<div>
+	<div class="drop-shadow">
 		<span class="font-bold uppercase">How to play:</span> Like BINGO but with a twist. Play as
 		usual, but to finish, make sure your marked squares connect and can make a swiggle from top to
 		bottom<sup class="font-super">*</sup>. Happy brewing!
 	</div>
-	<div class="text-xs">
+	<div class="drop-shadow text-xs">
 		<span class="font-super">*</span> This is currently generous and allows for any connection top-to-bottom.
 		Enjoy the dopamine!
 	</div>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,6 @@
-<h1 class="text-lg sm:text-2xl font-semibold font-serif text-cyan-50 uppercase text-center tracking-wider">
+<h1 class="text-lg sm:text-2xl font-semibold font-serif text-white drop-shadow uppercase text-center tracking-wider">
 	Tea Curious <span class="font-light">Tea Challenge</span>
 </h1>
-<h2 class="text-3xl sm:text-5xl font-bold font-serif text-cyan-50 text-center tracking-wider">
+<h2 class="text-3xl sm:text-5xl font-bold font-serif text-white drop-shadow text-center tracking-wider">
 	Moving Oolong!
 </h2>
